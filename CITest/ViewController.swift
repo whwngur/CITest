e7cc192c08558bafd5c671395e3a7d9b55aafa04
whwngur <dave@stra.ai>
2022-09-29 16:03:30 +0900

    Initial Commit

---
+++ CITest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CITest
+//
+//  Created by 조주혁 on 2022/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
