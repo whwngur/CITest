227ec5b2faae38032afececd5d510c94c10225be
whwngur <dave@stra.ai>
2022-10-06 17:10:02 +0900

    add UnitTest

---
+++ CITestTests/CITestTests.swift
@@ -0,0 +1,23 @@
+//
+//  CITestTests.swift
+//  CITestTests
+//
+//  Created by 조주혁 on 2022/10/06.
+//
+
+import XCTest
+
+final class CITestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssert(true)
+    }
+}
